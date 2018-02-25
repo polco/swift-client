491a994344c7954244cf91a6c50ccd2b26aaaeb2
Paul Colleoni <paul.colleoni@gmail.com>
2018-02-26 01:06:08 +0900

    Added the active user in the session display header

--- src/shared/views/SessionViewer.tsx
+++ src/shared/views/SessionViewer.tsx
@@ -1,8 +1,10 @@
+import { observer } from 'mobx-react';
 import * as React from 'react';
 
 import Button from 'shared/components/Button';
 import SessionDisplay from 'shared/components/SessionDisplay';
 import SessionInfo from 'shared/components/SessionInfo';
+import { Context, contextTypes } from 'shared/context';
 
 import './SessionViewer.less';
 
@@ -16,7 +18,11 @@ type State = {
 	infoOpen: boolean
 };
 
+@observer
 class SessionViewer extends React.Component<Props, State> {
+	public context!: Context;
+	public static contextTypes = contextTypes;
+
 	constructor(props: Props, context: any) {
 		super(props, context);
 
@@ -36,11 +42,13 @@ class SessionViewer extends React.Component<Props, State> {
 	public render() {
 		const sessionId = this.props.sessionId;
 		const infoOpen = this.state.infoOpen;
+		const session = this.context.store.getSession(sessionId);
 
 		return (
 			<div className='SessionViewer'>
 				<div className='SessionViewer__header'>
 					<Button onTap={ this.toggleInfo } className='SessionViewer__info'>Info</Button>
+					<div className='SessionViewer__user-count'>Users: { session.userIds.length }</div>
 					<Button onTap={ this.props.goBack } className='SessionViewer__go-back'>Go back</Button>
 					<div
 						className={ 'SessionViewer__slider-container' + (infoOpen ? '' : ' SessionViewer__slider-container_close') }
